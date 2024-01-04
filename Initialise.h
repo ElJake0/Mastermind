ede348ce89cb87137029b2cc36c31cd38e4edcb5
ElJake0 <jake.m.kent@gmail.com>
2024-01-04 21:29:44 +1100

    Added ability to use test argument when running:

---
+++ Initialise.h
@@ -0,0 +1,11 @@
+#ifndef INITIALISE_H
+#define INITIALISE_H
+
+// Include libraries
+#include <iostream>
+
+// Include Game variables
+#include "Board.h"
+#include "MenuUI.h"
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,31 +1,39 @@
-#include "Board.h"
-#include <iostream>
+#include "Initialise.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+  std::string argument;
   std::cout << "RUNNING MAIN" << std::endl;
 
-  // BOARD TEST
-  //---------------------------------------
-  std::cout << "Board test" << std::endl;
-  Board board = Board();
-  std::cout << "Board height = " << board.getBoardHeight() << std::endl;
-  std::cout << "Board width = " << board.getBoardWidth() << std::endl;
-  board.getBoardState();
+  if(argc > 1)
+  {
+    argument = argv[1];
+  }
 
-  std::cout << "Adding 'O' to board' at 5,3" << std::endl;
-  board.setBoardCoordinates(5,3,'O');
-  board.getBoardState();
+  if(argument == "test")
+  {
+    // BOARD TEST
+    //---------------------------------------
+    std::cout << "Board test" << std::endl;
+    Board board = Board();
+    std::cout << "Board height = " << board.getBoardHeight() << std::endl;
+    std::cout << "Board width = " << board.getBoardWidth() << std::endl;
+    board.getBoardState();
 
-  std::cout << "Adding 'O' to board' at 7,1" << std::endl;
-  board.setBoardCoordinates(7,1,'O');
-  board.getBoardState();
+    std::cout << "Adding 'O' to board' at 5,3" << std::endl;
+    board.setBoardCoordinates(5,3,'O');
+    board.getBoardState();
 
-  std::cout << "Checking board coordinates, expecting X: " << board.getBoardCoordinates(1,2) << std::endl;
-  std::cout << "Checking board coordinates, expecting O: " << board.getBoardCoordinates(5,3) << std::endl;
-  std::cout << "Checking board coordinates, expecting O: " << board.getBoardCoordinates(7,1) << std::endl;
-  //---------------------------------------
-  // END BOARD TEST
+    std::cout << "Adding 'O' to board' at 7,1" << std::endl;
+    board.setBoardCoordinates(7,1,'O');
+    board.getBoardState();
+
+    std::cout << "Checking board coordinates, expecting X: " << board.getBoardCoordinates(1,2) << std::endl;
+    std::cout << "Checking board coordinates, expecting O: " << board.getBoardCoordinates(5,3) << std::endl;
+    std::cout << "Checking board coordinates, expecting O: " << board.getBoardCoordinates(7,1) << std::endl;
+    //---------------------------------------
+    // END BOARD TEST
+  }
 
   return 0;
 }
